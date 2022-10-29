f738b9c5d03e9def85ea45f8cfa110379e2c78c9
unknown <aliogutcen@gmail.com>
2022-10-29 20:40:15 +0300

    gurayhoca

--- src/gurayhocaninodevleri/Homework002.java
+++ src/gurayhocaninodevleri/Homework002.java
@@ -5,12 +5,10 @@ public class Homework002 {
 	public static void main(String[] args) {
 		String metin = "Bugün Java ile string konusunu işleyeceğiz ";
 
-		/*
 		int name = metin.indexOf('i');
 		int lastName = metin.lastIndexOf('i');
 		System.out.print("i'nin geçtiği yerler= ");
 		while (name >= 0) {
-
 			if (name < lastName) {
 				System.out.print(name + ",");
 				name = metin.indexOf('i', name + 1);
@@ -19,19 +17,6 @@ public class Homework002 {
 				name = metin.indexOf('i', name + 1);
 			}
 		}
-		
-		*/
-		
-		
-        int i=0;
-        System.out.print("i harfinin geçtiği yerler:");
-        while(i<metin.length()) {
-            if(metin.charAt(i)=='i') {
-                System.out.print(" "+i+",");
-            }
-            i++;
-        }
-		
 
 	}
 
