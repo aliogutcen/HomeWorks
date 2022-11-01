1277e591a50b1f6336afa4fbe576a70cf724216d
unknown <aliogutcen@gmail.com>
2022-11-01 21:56:35 +0300

    Homework3

---
+++ src/homework3/Homework003.java
@@ -0,0 +1,24 @@
+package homework3;
+
+public class Homework003 {
+
+	public static void main(String[] args) {
+
+		int nums[] = { 1, 13, 13, 13, 2, 5, 13, 1, 13, 5, 12, 2 ,13,13,13,13,2,4,5};
+		int toplam = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 13) {
+				if (i != nums.length - 1) {
+					i++;
+					if(nums[i]==nums[i-1]) {
+						i--;
+					}
+				}
+			} else {
+				toplam += nums[i];
+			}
+		}
+		System.out.println("Toplam : " + toplam);
+
+	}
+}
